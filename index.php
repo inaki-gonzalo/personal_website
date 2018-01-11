a3fc5c6f4ec6050c6e3fd55744cb45cccc04e2e0
Inaki Gonzalo <iaminakigonzalo@gmail.com>
2018-01-11 12:04:36 -0800

    cleaned up the repo

--- index.php
+++ index.php
@@ -1,64 +1,64 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-    <!-- Bootstrap CSS -->
+    <title>Dev</title>
+
+   <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
   </head>
-  <body>
-	  
-	  <nav class="navbar navbar-expand navbar-dark bg-dark"> 
 
-
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent" >
-
-    <ul class="navbar-nav mr-auto" >
-
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="resume.pdf"  download="resume.pdf">Resume</a>
-       
-      </li>
-   
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Projects
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Web Stuff</a>
-          <a class="dropdown-item" href="#">Mechatronics</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Secrets</a>
-      </li>
-         <li class="nav-item">
-		<a class="nav-link" href="https://github.com/inaki-gonzalo" target="_blank" rel="noopener" aria-label="GitHub"><svg  height="16" class="navbar-nav-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 499.36" focusable="false"><title>GitHub</title><path d="M256 0C114.64 0 0 114.61 0 256c0 113.09 73.34 209 175.08 242.9 12.8 2.35 17.47-5.56 17.47-12.34 0-6.08-.22-22.18-.35-43.54-71.2 15.49-86.2-34.34-86.2-34.34-11.64-29.57-28.42-37.45-28.42-37.45-23.27-15.84 1.73-15.55 1.73-15.55 25.69 1.81 39.21 26.38 39.21 26.38 22.84 39.12 59.92 27.82 74.5 21.27 2.33-16.54 8.94-27.82 16.25-34.22-56.84-6.43-116.6-28.43-116.6-126.49 0-27.95 10-50.8 26.35-68.69-2.63-6.48-11.42-32.5 2.51-67.75 0 0 21.49-6.88 70.4 26.24a242.65 242.65 0 0 1 128.18 0c48.87-33.13 70.33-26.24 70.33-26.24 14 35.25 5.18 61.27 2.55 67.75 16.41 17.9 26.31 40.75 26.31 68.69 0 98.35-59.85 120-116.88 126.32 9.19 7.9 17.38 23.53 17.38 47.41 0 34.22-.31 61.83-.31 70.23 0 6.85 4.61 14.81 17.6 12.31C438.72 464.97 512 369.08 512 256.02 512 114.62 397.37 0 256 0z" fill="currentColor" fill-rule="evenodd"></path></svg>
-</a>
-      </li>
-
-    </ul>
-
+		<body>
+<div class="site-wrapper">
     
-  </div>
-</nav>
-    <h1>Hello, world!</h1>
+		<div class="outer">
+			<div class="inner">
+              <h3 class="masthead-brand">I&ntilde;aki Gonzalo</h3>
+              <nav class="nav nav-masthead">
+                <a class="nav-link active" href="/">Home</a>
+                 <a class="nav-link" href="resume.pdf"  download="resume.pdf">Resume</a>
+                <a class="nav-link" href="#">Projects</a>
+                 <a class="nav-link" href="#">Contact</a>
+                
+              </nav>
+            </div>
+          </header>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+          <main role="main" class="inner cover">
+			  <br></br>
+            <h1 class="cover-heading">Developer.</h1>
+            <br></br>
+            <img class="big_picture" src="background_dark.png" alt="Card image cap">
+            <br></br>
+            
+           
+          </main>
+
+          <footer class="mastfoot">
+            <div class="inner">
+               <p>Content copyright <?php echo date("Y"); ?>. I&ntilde;aki Gonzalo. All rights reserved.  </p>
+            </div>
+          </footer>
+
+   
+
+    </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
-  </body>
+   </body>
 </html>
